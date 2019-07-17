7cbbfd7302f9f222f5f6f3f236fc064880bd7f3a
GodSuperD <1653591454@qq.com>
2019-07-17 09:33:53 +0800

    天剑了TextF文件

---
+++ OCBaseFramework/OCBaseFramework/OCBaseFramework/TextF.h
@@ -0,0 +1,19 @@
+//
+//  TextF.h
+//  OCBaseFramework
+//
+//  Created by wangwei on 2019/7/17.
+//  Copyright © 2019 SuperD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TextF : NSObject
+
++ (void)share;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OCBaseFramework/OCBaseFramework/OCBaseFramework/TextF.m
@@ -0,0 +1,15 @@
+//
+//  TextF.m
+//  OCBaseFramework
+//
+//  Created by wangwei on 2019/7/17.
+//  Copyright © 2019 SuperD. All rights reserved.
+//
+
+#import "TextF.h"
+
+@implementation TextF
++ (void)share {
+    NSLog(@"Hello world!");
+}
+@end
